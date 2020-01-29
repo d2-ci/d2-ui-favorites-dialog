6c36dce44bd43e446733e756fbd64e4213cbdb33
GitHub <noreply@github.com>
2020-01-29 14:05:52 +0000

    fdd07fb fix: fix "Open in app" URL for visualization objects (#544)

---
+++ icons/RadarIcon.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _SvgIcon = require('@material-ui/core/SvgIcon');
+
+var _SvgIcon2 = _interopRequireDefault(_SvgIcon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _react2.default.createElement(
+    _SvgIcon2.default,
+    { viewBox: '0 0 16 16' },
+    _react2.default.createElement(
+        'g',
+        { stroke: 'none', strokeWidth: '1', fill: 'none', fillRule: 'evenodd' },
+        _react2.default.createElement('circle', { stroke: '#7B8998', cx: '8', cy: '8', r: '6' }),
+        _react2.default.createElement('circle', { stroke: '#7B8998', cx: '8', cy: '8', r: '3' }),
+        _react2.default.createElement('circle', { fill: '#7B8998', cx: '8', cy: '2', r: '2' }),
+        _react2.default.createElement('circle', { fill: '#7B8998', cx: '3', cy: '12', r: '2' })
+    )
+);
