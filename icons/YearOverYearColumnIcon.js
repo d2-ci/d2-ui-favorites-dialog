e22a6157f054a2d62c0c032ac8158144741aa51b
Edoardo Sabadelli <edoardo@dhis2.org>
2020-01-29 13:20:51 +0000

    0a99294 v6.5.2

---
+++ icons/YearOverYearColumnIcon.js
@@ -0,0 +1,34 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _SvgIcon = require('@material-ui/core/SvgIcon');
+
+var _SvgIcon2 = _interopRequireDefault(_SvgIcon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _react2.default.createElement(
+    _SvgIcon2.default,
+    { viewBox: '0 0 16 16' },
+    _react2.default.createElement(
+        'g',
+        { stroke: 'none', strokeWidth: '1', fill: 'none', fillRule: 'evenodd' },
+        _react2.default.createElement(
+            'g',
+            { fill: '#7B8998' },
+            _react2.default.createElement('rect', { x: '0', y: '15', width: '16', height: '1' }),
+            _react2.default.createElement('rect', { x: '0', y: '0', width: '1', height: '16' }),
+            _react2.default.createElement('rect', { x: '14', y: '9', width: '2', height: '6' }),
+            _react2.default.createElement('rect', { x: '11', y: '2', width: '2', height: '13' }),
+            _react2.default.createElement('rect', { x: '3', y: '11', width: '2', height: '4' }),
+            _react2.default.createElement('rect', { x: '6', y: '9', width: '2', height: '6' })
+        )
+    )
+);
