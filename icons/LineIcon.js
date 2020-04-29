d4cdb850b8949208c5d46dfeffc4a65f00cc74f3
Jen Jones Arnesen <jennifer@dhis2.org>
2020-04-29 16:44:13 +0000

    961b56b fix: favorites-dialog needs transifex config

---
+++ icons/LineIcon.js
@@ -0,0 +1,36 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _SvgIcon = require('@material-ui/core/SvgIcon');
+
+var _SvgIcon2 = _interopRequireDefault(_SvgIcon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _react2.default.createElement(
+    _SvgIcon2.default,
+    { viewBox: '0 0 16 16', style: { fontSize: 16 } },
+    _react2.default.createElement(
+        'g',
+        { stroke: 'none', strokeWidth: '1', fill: 'none', fillRule: 'evenodd' },
+        _react2.default.createElement(
+            'g',
+            null,
+            _react2.default.createElement(
+                'mask',
+                { id: 'mask-2', fill: 'white' },
+                _react2.default.createElement('rect', { x: '0', y: '0', width: '16', height: '16' })
+            ),
+            _react2.default.createElement('rect', { fill: '#7B8998', mask: 'url(#mask-2)', x: '0', y: '15', width: '16', height: '1' }),
+            _react2.default.createElement('rect', { fill: '#7B8998', mask: 'url(#mask-2)', x: '0', y: '0', width: '1', height: '16' }),
+            _react2.default.createElement('polyline', { stroke: '#7B8998', strokeWidth: '1.5', mask: 'url(#mask-2)', points: '0 5 5 9 9 7 15 12' })
+        )
+    )
+);
