4d32005fb093c472d6bae66a76092cc1f9ef5549
Jen Jones Arnesen <jennifer@dhis2.org>
2020-05-06 17:40:53 +0000

    e2e722b v7.0.2

---
+++ icons/StackedColumnIcon.js
@@ -0,0 +1,36 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _SvgIcon = require('@material-ui/core/SvgIcon');
+
+var _SvgIcon2 = _interopRequireDefault(_SvgIcon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _react2.default.createElement(
+    _SvgIcon2.default,
+    { viewBox: '0 0 16 16', style: { fontSize: 16 } },
+    _react2.default.createElement(
+        'g',
+        { stroke: 'none', strokeWidth: '1', fill: 'none', fillRule: 'evenodd' },
+        _react2.default.createElement(
+            'g',
+            { fill: '#7B8998' },
+            _react2.default.createElement('rect', { x: '0', y: '15', width: '16', height: '1' }),
+            _react2.default.createElement('rect', { x: '0', y: '0', width: '1', height: '16' }),
+            _react2.default.createElement('rect', { x: '13', y: '8', width: '3', height: '7' }),
+            _react2.default.createElement('rect', { x: '13', y: '4', width: '3', height: '3' }),
+            _react2.default.createElement('rect', { x: '8', y: '9', width: '3', height: '6' }),
+            _react2.default.createElement('rect', { x: '8', y: '1', width: '3', height: '7' }),
+            _react2.default.createElement('rect', { x: '3', y: '13', width: '3', height: '2' }),
+            _react2.default.createElement('rect', { x: '3', y: '7', width: '3', height: '5' })
+        )
+    )
+);
