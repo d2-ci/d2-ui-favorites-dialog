78f77164b16905283e434348932fa681641d07ab
GitHub <noreply@github.com>
2020-09-08 14:59:15 +0000

    422ef30 fix: lock the Dialog width so it doesn't jump when paginating (#627)

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+require("./locales/");
+
+var _FavoritesDialog = require("./FavoritesDialog");
+
+var _FavoritesDialog2 = _interopRequireDefault(_FavoritesDialog);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _FavoritesDialog2.default;
